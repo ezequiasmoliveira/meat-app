6f82e54d951a6a44be0e48f0d9b51d78f9e24cc1
ezequias.oliveira <esequias.m.oliveira@gmail.com>
2018-07-30 18:52:10 -0300

    criado o input com as validações de obrigatóriedade de preenchimento. e corrigida a implementad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { RestaurantService } from './restaurants/restaurants.service';
 import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component';
 
 
 @NgModule({
@@ -36,7 +37,8 @@ import { OrderComponent } from './order/order.component';
     MenuComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
