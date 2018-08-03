ca72b59d6381275ec81c0bfc578a05390e8ec3c9
ezequias.oliveira <esequias.m.oliveira@gmail.com>
2018-08-03 11:55:04 -0300

    refatoração no coreModule, implementado o pré load dos modulos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
-import { CoreModule } from './core/core.module';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import { ROUTES } from './app.routes';
 
@@ -40,9 +38,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     HttpModule,
-    SharedModule,
-    CoreModule,
-    RouterModule.forRoot(ROUTES)
+    SharedModule.forRoot(),
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'pt-BR'}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -6,10 +6,21 @@ import { RatingComponent } from './rating/rating.component';
 import { RadioComponent } from './radio/radio.component';
 import { InputComponent } from './input/input.component';
 
+import { OrderService } from './../core/service/order.service';
+import { RestaurantService } from './../core/service/restaurants.service';
+import { ShoppingCartService } from './../core/service/shopping-cart.service';
+
 @NgModule({
   declarations: [InputComponent, RadioComponent, RatingComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [InputComponent, RadioComponent, RatingComponent,
             CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class SharedModule {}
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [ShoppingCartService, RestaurantService, OrderService]
+    };
+  }
+}
