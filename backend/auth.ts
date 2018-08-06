0dd875fd6b7a5b5f24ea5a227b1c229cb0b51d9f
ezequias.oliveira <esequias.m.oliveira@gmail.com>
2018-08-06 17:16:14 -0300

    implementado o backend para autenticação, e autorização.

---
+++ backend/auth.ts
@@ -0,0 +1,25 @@
+import { User, users } from './user';
+import { Request, Response } from 'express';
+
+import * as jwt from 'jsonwebtoken'
+
+import { apiConfig } from './api-config';
+
+export const handleAuthentication = (req: Request, resp: Response) => {
+  const user: User = req.body;
+  if (isValid(user)) {
+    const dbUser = users[user.email]
+    const token = jwt.sign({ subject: dbUser.email, issuer: 'meat-api'}, apiConfig.secret);
+    resp.json ({name: dbUser.name, email: dbUser.email, accessToken: token})
+  } else {
+    resp.status(403).json({message: 'Dados inválidos.'})
+  }
+}
+
+function isValid(user: User): boolean {
+  if (!user) {
+    return false;
+  }
+  const dbUser = users[user.email];
+  return dbUser !== undefined && dbUser.matches(user);
+}
